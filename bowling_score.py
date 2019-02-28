04dced253f0c82afedcdabac352e494e7fc80cce
Chad Quickstad <cquickstad@gmail.com>
2019-02-27 17:27:16 -0800

    Refactor spare logic

--- bowling_score.py
+++ bowling_score.py
@@ -4,13 +4,21 @@
 
 class BowlingScoreCalculator:
     def __init__(self):
+        self._frames = None
+        self._frame_idx = None
         self._frame = None
 
     def score_game(self, frames):
+        self._frames = frames
         score = 0
-        for i, self._frame in enumerate(frames):
+        for self._frame_idx, self._frame in enumerate(frames):
             score += sum(self._frame)
-            if sum(self._frame) == 10:  # spare
-                score += frames[i+1][0]
+            score += self._get_next_throw() if self._is_spare() else 0
         return score
+
+    def _get_next_throw(self):
+        return self._frames[self._frame_idx+1][0]
+
+    def _is_spare(self):
+        return sum(self._frame) == 10
 
